1a6129df5340a6d8a7e6364b738af1abcde31b0b
pbruyninckx <pbruyninckx@gmail.com>
2019-12-16 23:46:23 +0000

    Day 12 part 2: Find the period
    The overuse of structs in the wrong locations might render
    this a little bit slow.

--- src/n_body.clj
+++ src/n_body.clj
@@ -27,6 +27,8 @@
                  [2 -7 3]
                  [9 -8 -3]])))
 
+(defn select-dimension [moons dim]
+  (mapv ->body (select [ALL :pos dim] moons) (select [ALL :vel dim] moons)))
 
 (defn update-velocities [moons]
   (let [pos (select [ALL :pos] moons)
@@ -45,6 +47,22 @@
   (* (reduce + (map #(Math/abs %) (:pos moon)))
      (reduce + (map #(Math/abs %) (:vel moon)))))
 
+(defn iterate-1d [moons]
+  (let [pos (select [ALL :pos] moons)
+        vel (select [ALL :vel] moons)
+        vel-delta
+        (map #(reduce + %) (partition 4 (for [p1 pos p2 pos] (compare p2 p1))))
+        new-vel (mapv + vel vel-delta)
+        new-pos (mapv + pos new-vel)]
+    (mapv ->body new-pos new-vel)))
+
+(defn find-period [orig-moon]
+  (loop [moon (iterate-1d orig-moon)
+         step 1]
+    (if (= moon orig-moon)
+      step
+      (recur (iterate-1d moon) (inc step)))))
+
 (def simulate-step (comp update-positions update-velocities))
 
 (defn simulate [n moons]
@@ -53,3 +71,16 @@
 (defn total-energy [moons]
   (reduce + (map energy moons)))
 
+(defn gcd
+  "(gcd a b) computes the greatest common divisor of a and b."
+  [a b]
+  (if (zero? b)
+    a
+    (recur b (mod a b))))
+(defn lcm
+  [a b]
+  (/ (* a b) (gcd a b)))
+;; to calculate the lcm for a variable number of arguments
+(defn lcmv [& v] (reduce lcm v))
+
+(println (apply lcmv (map #(find-period (select-dimension moons %)) (range 3))))
